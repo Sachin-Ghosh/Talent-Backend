5bbfbbd9a83c0bbf8c7e84a9da5abf781e8810a0
SACHIN <ghoshsachin2301@gmail.com>
2024-11-13 23:54:45 +0530

    some changes

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,25 +1,5 @@
 const nodemailer = require('nodemailer');
 
-// // Create a test account if you don't have a real email service configured
-// let transporter;
-
-// async function createTestAccount() {
-//   let testAccount = await nodemailer.createTestAccount();
-  
-//   transporter = nodemailer.createTransport({
-//     host: "smtp.ethereal.email",
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//       user: testAccount.user, // generated ethereal user
-//       pass: testAccount.pass, // generated ethereal password
-//     },
-//   });
-// }
-
-// createTestAccount();
-
-// If you have a real email service, use this configuration instead:
 const transporter = nodemailer.createTransport({
   service: 'gmail',  // or your email service
   host: "smtp.gmail.com",
